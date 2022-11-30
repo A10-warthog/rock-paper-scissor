5a596bdca032e371fce6f5cef1469d8b0118d2f1
A10-warthog <behemoth11111111@gmail.com>
2022-11-30 23:30:06 +0530

    Remove eventListener from buttons

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 "use strict"
 
- 
+let int_userCount = 0, int_computerCount = 0;
+
+const run_count = document.querySelector(".run_count");
 const string_userCount = document.querySelector(".run_count__user");
 const string_computerCount = document.querySelector(".run_count__computer"); 
 const result = document.querySelector(".result");
 const userChoice = document.querySelector(".user_option").children;
 
-let int_userCount = 0, int_computerCount = 0;
-
-button.forEach(button => button.addEventListener("click", getPlayerChoice));
-
 function getComputerChoice() {
     const int_choice = Math.floor(Math.random() * 3);
     const string_choice = ["Rock", "Paper", "Scissor"];
-
     return string_choice[int_choice];
 }
 
